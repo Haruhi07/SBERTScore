904eacaa9d238f35ad4e6ab1460e011a02e423ab
Yuxuan Ye <hs20307@it045310.wks.bris.ac.uk>
2024-06-10 18:22:30 +0100

    speed test

--- speed.py
+++ speed.py
@@ -1,8 +1,8 @@
 import os
 import json
+import time
 import pandas as pd
 import evaluate
-from tqdm import tqdm
 from evaluation_utils import sbert_scorer
 
 result_dir = "results.json"
@@ -12,62 +12,73 @@ metric_list = ["ROUGE-1-F", "ROUGE-2-F", "ROUGE-L-F", "SBERTScore-P"]
 summarisers_to_eval = "margin-bart-large-cnn"
 summary_file_suffix = ".json" # {"id": id, "summary": summary}
 
-data_df = pd.read_json("cnn_test.json")
+df = pd.read_csv('aggre_fact_final_main-100.csv')
+df = df[:1000]
 
+source = df["doc"].to_list()
+summary = df["summary"].to_list()
 
-json_dir = f"{summary_dir}/{summarisers_to_eval+summary_file_suffix}"
-with open(json_dir, "r") as fp:
-    sum_df = pd.read_json(json_dir)
+rouge_scorer = evaluate.load('rouge')
+scorer = sbert_scorer.SBERT_Scorer()
 
-main_df = pd.merge(data_df, sum_df, on="id")
+st = time.time()
+sbert_score, _, __= scorer.compute(summaries=summary, targets=source)
+ed = time.time()
 
-source = main_df["source"].to_list()
-reference = main_df["reference"].to_list()
-summary = main_df["summary"].to_list()
+running_time = ed - st
+df["SBERTScore-P"] = sbert_score
+
+print(f"SBERT Speed: {10066/running_time*60}")
 
 import sys
 sys.path.append("./evaluation_utils/QuestEval")
 from questeval.questeval_metric import QuestEval
+from tqdm import tqdm
 questeval = QuestEval(task="summarization", do_weighter=True)
 
-list_references = [[ref] for ref in reference]
+st = time.time()
 score = questeval.corpus_questeval(
     hypothesis=summary, 
     sources=source,
-    list_references=list_references,
     batch_size=32,
 )
-print(score["corpus_score"])
-main_df["QuestEval"] = score["ex_level_scores"]
+ed = time.time()
+running_time = ed - st
+print(10066*60/running_time)
+df["QuestEval"] = score["ex_level_scores"]
 
-rouge_scorer = evaluate.load('rouge')
-sbert_scorer = sbert_scorer.SBERT_Scorer()
-
-rouge_score = rouge_scorer.compute(predictions=summary, references=reference, use_stemmer=True)
-main_df["ROUGE-1-F"] = rouge_score["rouge1"]
-main_df["ROUGE-2-F"] = rouge_score["rouge2"]
-main_df["ROUGE-L-F"] = rouge_score["rougeLsum"]
-
-print(sum(main_df["ROUGE-1-F"])/len(main_df)) # 44.05 43.37
-print(sum(main_df["ROUGE-2-F"])/len(main_df)) # 21.07 20.27
-print(sum(main_df["ROUGE-L-F"])/len(main_df)) # 37.36 40.39
-
-sbert_score, _, __= sbert_scorer.compute(summaries=summary, targets=source)
-main_df["SBERTScore-P"] = sbert_score
-
-print(sum(main_df["SBERTScore-P"])/len(main_df))
-
-from summac.model_summac import SummaCConv, SummaCZS
-
-model_zs = SummaCZS(models=["vitc"], bins='percentile', granularity="sentence", nli_labels="e", device="cuda", start_file="default", agg="mean")
-
-summac_score = []
-for doc, summ in zip(source, tqdm(summary)):
-    summac_score.append(model_zs.score([doc], [summ])["scores"][0])
-
+import sys
+sys.path.append("./evaluation_utils/summac")
+from summac.model_summac import SummaCZS, SummaCConv
+from tqdm import tqdm
 
 model_conv = SummaCConv(models=["vitc"], bins='percentile', granularity="sentence", nli_labels="e", device="cuda", start_file="default", agg="mean")
 
 summac_score = []
+st = time.time()
 for doc, summ in zip(source, tqdm(summary)):
     summac_score.append(model_conv.score([doc], [summ])["scores"][0])
+ed = time.time()
+
+running_time = ed - st
+df["SummaC-Conv"] = summac_score
+
+
+print(f"SummaC-Conv Speed: {10066*60/running_time}")
+
+
+from summac.model_summac import SummaCZS, SummaCConv
+from tqdm import tqdm
+
+st = time.time()
+model_conv = SummaCZS(models=["anli"], bins='percentile', granularity="sentence", nli_labels="e", device="cuda", start_file="default", agg="mean", imager_load_cache=False)
+
+summac_score = []
+for doc, summ in zip(source, tqdm(summary)):
+    summac_score.append(model_conv.score([doc], [summ])["scores"][0])
+ed = time.time()
+
+running_time = ed - st
+df["SummaC-ZS"] = summac_score
+
+print(f"SummaC-Conv Speed: {10066*60/running_time}")
